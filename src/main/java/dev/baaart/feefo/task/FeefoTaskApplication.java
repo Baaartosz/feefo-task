887f67dd265a15c895c83d1470b54c1574bacda7
Bartosz Perczynski <bartosz_p@hotmail.com>
2025-01-18 09:58:19 +0000

    Initialize Spring Boot project

---
+++ src/main/java/dev/baaart/feefo/task/FeefoTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.baaart.feefo.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeefoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeefoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/baaart/feefo/task/FeefoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.baaart.feefo.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeefoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
